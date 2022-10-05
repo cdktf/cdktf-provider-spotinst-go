b8a9a0b55672cdb2978b7c07da4fe5ce14c9a309
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:33:45 +0000

    chore(release): v3.0.2

--- spotinst/elastigroupawsbeanstalk/elastigroupawsbeanstalk_ElastigroupAwsBeanstalk.go
+++ spotinst/elastigroupawsbeanstalk/elastigroupawsbeanstalk_ElastigroupAwsBeanstalk.go
@@ -2,10 +2,10 @@ package elastigroupawsbeanstalk
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-spotinst-go/spotinst/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-spotinst-go/spotinst/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-spotinst-go/spotinst/v3/elastigroupawsbeanstalk/internal"
+	"github.com/cdktf/cdktf-provider-spotinst-go/spotinst/v3/elastigroupawsbeanstalk/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- spotinst/subscription/subscription_Subscription.go
+++ spotinst/subscription/subscription_Subscription.go
@@ -2,10 +2,10 @@ package subscription
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-spotinst-go/spotinst/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-spotinst-go/spotinst/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-spotinst-go/spotinst/v3/subscription/internal"
+	"github.com/cdktf/cdktf-provider-spotinst-go/spotinst/v3/subscription/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
