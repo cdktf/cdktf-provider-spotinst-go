06c71bd38e0deaecf08df66f2d51894264563f45
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-12-04 04:05:46 +0000

    chore(release): v13.35.2

--- spotinst/elastigroupaws/ElastigroupAwsIntegrationBeanstalk.go
+++ spotinst/elastigroupaws/ElastigroupAwsIntegrationBeanstalk.go
@@ -7,13 +7,13 @@ package elastigroupaws
 type ElastigroupAwsIntegrationBeanstalk struct {
 	// deployment_preferences block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.2/docs/resources/elastigroup_aws#deployment_preferences ElastigroupAws#deployment_preferences}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.3/docs/resources/elastigroup_aws#deployment_preferences ElastigroupAws#deployment_preferences}
 	DeploymentPreferences *ElastigroupAwsIntegrationBeanstalkDeploymentPreferences `field:"optional" json:"deploymentPreferences" yaml:"deploymentPreferences"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.2/docs/resources/elastigroup_aws#environment_id ElastigroupAws#environment_id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.3/docs/resources/elastigroup_aws#environment_id ElastigroupAws#environment_id}.
 	EnvironmentId *string `field:"optional" json:"environmentId" yaml:"environmentId"`
 	// managed_actions block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.2/docs/resources/elastigroup_aws#managed_actions ElastigroupAws#managed_actions}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.3/docs/resources/elastigroup_aws#managed_actions ElastigroupAws#managed_actions}
 	ManagedActions *ElastigroupAwsIntegrationBeanstalkManagedActions `field:"optional" json:"managedActions" yaml:"managedActions"`
 }
 
--- spotinst/elastigroupgke/ElastigroupGkeIntegrationDockerSwarm.go
+++ spotinst/elastigroupgke/ElastigroupGkeIntegrationDockerSwarm.go
@@ -5,9 +5,9 @@ package elastigroupgke
 
 
 type ElastigroupGkeIntegrationDockerSwarm struct {
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.2/docs/resources/elastigroup_gke#master_host ElastigroupGke#master_host}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.3/docs/resources/elastigroup_gke#master_host ElastigroupGke#master_host}.
 	MasterHost *string `field:"required" json:"masterHost" yaml:"masterHost"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.2/docs/resources/elastigroup_gke#master_port ElastigroupGke#master_port}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/spotinst/spotinst/1.199.3/docs/resources/elastigroup_gke#master_port ElastigroupGke#master_port}.
 	MasterPort *float64 `field:"required" json:"masterPort" yaml:"masterPort"`
 }
 
